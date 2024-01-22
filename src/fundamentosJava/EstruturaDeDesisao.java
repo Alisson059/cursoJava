c8e158b7912ca18c37337c033fe47334797b9869
Alisson059 <alissonmelo059@gmail.com>
2024-01-22 11:27:50 -0300

    Estudo de varáveis, operações aritiméticas e atribuições, operações relacionais e lógicos. Uso de estrutura de desisão

---
+++ src/fundamentosJava/EstruturaDeDesisao.java
@@ -0,0 +1,36 @@
+package fundamentosJava;
+
+public class EstruturaDeDesisao {
+
+	public static void main(String[] args) {
+
+		String nome = "joão Roberto";
+		int idade = 22;
+		char sexo = 'M';
+ 
+		System.out.println("__________________________________________.");
+ 
+		if (sexo == 'M') {
+ 
+			if (idade < 18) {
+ 
+				System.out.println("Voce não tem idade necessaria.");
+ 
+			}
+ 
+			else {
+				System.out.println("Alistamento obrigatório");
+			}
+ 
+		}
+		
+		
+ 
+		else {
+ 
+			System.out.println("Alistamento negado.");
+		}
+ 
+	}
+
+}
